30b7cb9689feec644196e89057ac11bf9616b7bc
edibeca <010200837k@uandina.edu.pe>
2022-01-29 16:18:19 -0500

    formulas para triangulo y cuadrado

---
+++ js.js
@@ -0,0 +1,51 @@
+
+console.group("Cuadrado");
+function pericua(){
+    const input=document.getElementById("Inputcua");
+    const lado=input.value;
+    let perimetro=4*lado;
+    alert(perimetro);
+}
+function areacua(){
+    const input=document.getElementById("Inputcua");
+    const lado=input.value;
+    let area=lado**2;
+    alert(area);
+}
+console.groupEnd();
+console.group("Triangulo");
+function peritri(){
+    const input1=document.getElementById("Input1");
+    const lado1=input1.value;
+    const input2=document.getElementById("Input2");
+    const lado2=input2.value;
+    const input3=document.getElementById("Input3");
+    const lado3=input3.value;
+    const suma=(parseInt(lado1) + parseInt(lado2) + parseInt(lado3));
+    let perimetro = suma;
+    alert(perimetro);
+}
+function areatri(){
+    const input1=document.getElementById("Input1");
+    const lado1=input1.value;
+    const input2=document.getElementById("Input2");
+    const lado2=input2.value;
+    const input3=document.getElementById("Input3");
+    const lado3=input3.value;
+    const semip=(parseInt(lado1) + parseInt(lado2) + parseInt(lado3))/2;
+    const semiarea=(semip*(semip-lado1)*(semip-lado2)*(semip-lado3));
+    let area=Math.sqrt(semiarea);
+    alert(area);
+
+}
+console.groupEnd();
+console.group("Circulo");
+function perimetrocir(radio){
+    let perimetro = (2*(Math.PI))*radio;
+    return perimetro;
+}
+function areacir(radio){
+    let area=(Math.PI)*(radio**2);
+    return area;
+}
+console.groupEnd();
