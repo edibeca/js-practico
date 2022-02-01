47f0ceb8675eff6dbe0b566941e2d7a201d15f03
edibeca <010200837k@uandina.edu.pe>
2022-01-31 20:17:34 -0500

    Intento de cupones

--- des.js
+++ des.js
@@ -17,3 +17,14 @@ function calcularPrecioConDescuento(precio, descuento) {
     const resultP = document.getElementById("ResultP");
     resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
   }
+
+  function cupones(precio,cupon){
+    let arr=["10%","20%","30%"];
+    for (let i = 0;  i < arr.length; i++) {
+      const element = arr[i];
+      if (cupon=="10%") {
+        const desc=(precio*10)/100;
+        console.log("El descuento es: " + desc);
+      }
+    }
+  }
